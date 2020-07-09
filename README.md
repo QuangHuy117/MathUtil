38048c926bfb08d46766fa8125eeea932d2fbd61
QuangHuy117 <tranvanquanghuy117@gmail.com>
2020-07-09 13:37:46 +0700

    1st: first upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to Quang Huy repository
+This repository contains the source code of a simple Java app. It is used to demonstrate
+the CI - Continuous Integration process, a part of Introduction to Softwate Engineering
+course at FPT University HCMC Campus
+
+## You will find in this repo:
+* The MathUtil class (consists of multiple Math methods)
+* Unit Testing by JUnit (for TDD technic) 
+* Build configuration file using for Jenkins
+
+## Contact me via:
+1.[GitHub](http://github.com/QuangHuy117)
+2.[Facebook](https://facebook.com/Quang Huy)
+
+#### © 2020 by Quang Huy
---
+++ src/huy/util/MathUtil.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huy.util;
+
+/**
+ *
+ * @author Admin
+ */
+//đây là class chứa các hàm tiện ích xài chung cho các nơi khác
+//nó chứa các hàm tính toán bổ trợ cho các class khác
+//do là đồ bổ trợ, xài chung, nên chúng là STATIC
+public class MathUtil {
+    
+    //hàm tính n! = 1.2.3.4..n, vì tăng kết quả rất nhanh, sớm tràn
+    //int chỉ tối đa 2 tỷ 1, phải để long cho đỡ bị tràn kết quả
+    //mình chỉ nên tính n <= 15, vì 20! to lắm
+    public static long computerFactorial(int n) {
+        
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        
+        return result;
+    }
+    
+}
